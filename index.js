c64f6e22b4a57bab2e2949364d95a28dbd6caca4
Sarvesh Shirude <102407237+esarvesh11@users.noreply.github.com>
2022-07-07 13:58:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png"); //images/dice1.png - images/dice6.png
 
+//Player 1 Wins
 if(randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
 }
+//Player 2 WIns
 else if(randomNumber1 < randomNumber2){
     document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
 }
+//Draw
 else{
     document.querySelector("h1").innerHTML = "Draw!📍";
 }
